717b4c50fece62ad9e02df871cc7e491a77d4801
TheAustrian <villacherbua@gmail.com>
2016-03-05 14:52:11 +0100

    Create LEDWebsockets.h
    Initial commit.

---
+++ arduino/LEDWebsockets.h
@@ -0,0 +1,93 @@
+/* Based on the websocket example and 
+the code here http://www.whatimade.today/esp8266-on-websockets-mdns-ota-and-leds/ */
+
+WebSocketsServer webSocket = WebSocketsServer(81);
+
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t lenght) {
+
+    switch(type) {
+        case WStype_DISCONNECTED:
+            //USE_SERIAL.printf("[%u] Disconnected!\n", num);
+            break;
+        case WStype_CONNECTED:
+            {
+                IPAddress ip = webSocket.remoteIP(num);
+                //USE_SERIAL.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
+        
+        // send message to client
+        String websocketStatusMessage = "H" + String(myHue) + ",S" + String(mySaturation) + ",V" + String(myValue); //Sends a string with the HSV values to the client website when the conection gets established
+        webSocket.sendTXT(num, websocketStatusMessage);
+        
+        webSocket.sendTXT(num, ESP.getResetInfo()); //Handy for debugging
+            }
+            break;
+        case WStype_TEXT:
+            {
+              //USE_SERIAL.printf("[%u] get Text: %s\n", num, payload);
+            
+
+            // send message to client
+            // webSocket.sendTXT(num, "message here");
+
+            // send data to all connected clients
+            // webSocket.broadcastTXT("message here");
+
+            String text = String((char *) &payload[0]);
+
+             if (text.startsWith("a")) {
+               String xVal = (text.substring(text.indexOf("a") + 1, text.length()));
+                flickerLed = random(0,29);
+                if (myEffect != xVal.toInt()) {  // only do stuff when there was a change
+                myEffect = xVal.toInt();
+                rainbowHue = myHue;
+                EEPROM.write(0, myEffect);       //stores the variable but needs to be committed to EEPROM before being saved - this happens in the loop
+                lastChangeTime = millis();
+                eepromCommitted = false;
+                }
+             }
+             if (text.startsWith("b")) {
+               String xVal = (text.substring(text.indexOf("b") + 1, text.length()));
+                if (myHue != xVal.toInt()) {
+                myHue = xVal.toInt();
+                rainbowHue = myHue;
+                EEPROM.write(1, myHue);
+                lastChangeTime = millis();
+                eepromCommitted = false;
+                }
+             }
+              if (text.startsWith("c")) {
+               String xVal = (text.substring(text.indexOf("c") + 1, text.length()));
+                if (mySaturation != xVal.toInt()) {
+                mySaturation = xVal.toInt();
+                EEPROM.write(2, mySaturation);
+                lastChangeTime = millis();
+                eepromCommitted = false;
+                }
+             }
+              if (text.startsWith("d")) {
+               String xVal = (text.substring(text.indexOf("d") + 1, text.length()));
+                if (myValue != xVal.toInt()) {
+                myValue = xVal.toInt();
+                EEPROM.write(3, myValue);
+                lastChangeTime = millis();
+                eepromCommitted = false;
+                }
+             }
+             /*currentTime = millis();
+              if (currentTime - previousTime > 1000) {
+                String websocketStatusMessage = "H" + String(myHue) + ",S" + String(mySaturation) + ",V" + String(myValue);
+                webSocket.broadcastTXT(websocketStatusMessage);
+                previousTime = currentTime;
+               }*/
+            break;
+            }
+        case WStype_BIN:
+            //USE_SERIAL.printf("[%u] get binary lenght: %u\n", num, lenght);
+            hexdump(payload, lenght);
+
+            // send message to client
+            // webSocket.sendBIN(num, payload, lenght);
+            break;
+    }
+
+}
